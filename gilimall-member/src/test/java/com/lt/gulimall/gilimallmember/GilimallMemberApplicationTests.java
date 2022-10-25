1b6cf39852d4c56898200c5633fdcc3ae8253726
q1746397845 <1746397845@qq.com>
2022-10-25 16:17:11 +0800

    初始化项目

---
+++ gilimall-member/src/test/java/com/lt/gulimall/gilimallmember/GilimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lt.gulimall.gilimallmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GilimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
